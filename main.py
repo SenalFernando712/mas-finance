d286e34cb5508e9f907683a4a8a1d5548765ad79
Senal Fernando <166195579+SenalFernando712@users.noreply.github.com>
2024-04-24 16:20:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ def main():
     st.title('Display CSV Data from GitHub')
 
     # Replace 'raw_github_csv_link' with the raw GitHub link to your CSV file
-    raw_github_csv_link = 'https://raw.githubusercontent.com/SenalFernando712/mas-finance/main/GL_list1.csv'
+    raw_github_csv_link_gl = 'https://raw.githubusercontent.com/SenalFernando712/mas-finance/main/GL_list1.csv'
 
     # Read CSV data
     try:
-        df = read_csv_data(raw_github_csv_link)
-        st.write('### First 5 rows of the CSV file:')
-        st.write(df.head())
+        df = read_csv_data(raw_github_csv_link_gl)
+        gl_codes = df['Column 1'].astype(str) + ' : ' + df['Column 2'].astype(str)
+        gl_no = st.selectbox('GL No', gl_codes)
+        st.write('Selected GL No:', gl_no)
     except Exception as e:
         st.error(f'Error reading CSV file: {e}')
 
